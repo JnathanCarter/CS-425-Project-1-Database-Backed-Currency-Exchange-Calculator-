44a7305f5129020c1f92341577401f58a2a7f937
JnathanCarter <jcarter20@stu.jsu.edu>
2022-11-14 00:59:51 -0600

    init

---
+++ 425-Project-1/src/java/edu/jsu/mcis/dao/RateDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.jsu.mcis.dao;
+
+/**
+ *
+ * @author johnc
+ */
+public class RateDAO {
+    
+   private final DAOFactory daoFactory;
+   
+    RateDAO(DAOFactory daoFactory){
+        this.daoFactory = daoFactory;
+    }
+
+}
