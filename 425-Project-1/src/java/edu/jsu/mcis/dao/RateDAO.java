c7ba4fc0aa51f013772367b417f7cc2d2ca2876b
JnathanCarter <jcarter20@stu.jsu.edu>
2022-11-15 01:42:23 -0600

    jd

--- 425-Project-1/src/java/edu/jsu/mcis/dao/RateDAO.java
+++ 425-Project-1/src/java/edu/jsu/mcis/dao/RateDAO.java
@@ -4,12 +4,23 @@
  */
 package edu.jsu.mcis.dao;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import javax.swing.text.AbstractDocument.Content;
+import javax.swing.text.html.HTMLDocument.RunElement;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -24,6 +35,8 @@ public class RateDAO {
         private final DAOFactory daoFactory;
         private final String QUERY_SELECT = "SELECT * FROM rate WHERE rate_date = ?";
         private final String QUERY_EXTERNAL_DB = "https://testbed.jaysnellen.com:8443/JSUExchangeRatesServer/rates";
+        private final String QUERY_CREATE = "INSERT INTO rate (currencyid, rate_date, rate)"
+                        + "VALUES (?,?,?)";
 
         RateDAO(DAOFactory daoFactory) {
                 this.daoFactory = daoFactory;
@@ -98,6 +111,132 @@ public class RateDAO {
         }
 
         public String updateInternalDB(String date) {
-                return "this wasnt in db";
+
+                // request from external api
+                Connection conn = daoFactory.getConnection();
+                PreparedStatement ps = null;
+                ResultSet rs = null;
+
+                try {
+                    // get information from external api
+                        URL url = new URL("https://testbed.jaysnellen.com:8443/JSUExchangeRatesServer/rates");
+                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                        con.setRequestMethod("GET");
+
+                        BufferedReader in = new BufferedReader(new InputStreamReader(
+                        con.getInputStream()));
+                        String inputLine;
+                        String output = "";
+                        while ((inputLine = in.readLine()) != null) {
+                        System.out.println(inputLine);
+                                                 output = inputLine;
+
+                        }
+                        System.out.println("output-----------"+output);
+                        in.close();
+                        
+                    //parse the output 
+                    
+                    //add to internal db
+
+
+
+                } catch (Exception e) {
+                        e.printStackTrace();
+                } finally {
+
+                        if (rs != null) {
+                                try {
+                                        rs.close();
+                                        rs = null;
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                }
+                        }
+                        if (ps != null) {
+                                try {
+                                        ps.close();
+                                        ps = null;
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                }
+                        }
+                        if (conn != null) {
+                                try {
+                                        conn.close();
+                                        conn = null;
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                }
+                        }
+
+                }
+                // instead of adding to json object add each entry to the database
+
+                // call find again
+                // return this.find(date);
+                return "";
+        }
+
+        private void addCurrencyData(String date, String currencyId, Double rate) {
+
+                PreparedStatement ps = null;
+                ResultSet rs = null;
+
+                try {
+                        Connection conn = daoFactory.getConnection();
+
+                        ps = conn.prepareStatement(QUERY_CREATE);
+                        ps.setString(1, currencyId);
+                        ps.setString(2, date);
+                        ps.setDouble(3, rate);
+
+                        int updateCount = ps.executeUpdate();
+
+                        if (updateCount > 0) {
+                                return;
+                        }
+
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
+
+                finally {
+
+                        if (rs != null) {
+                                try {
+                                        rs.close();
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                }
+                        }
+                        if (ps != null) {
+                                try {
+                                        ps.close();
+                                } catch (Exception e) {
+                                        e.printStackTrace();
+                                }
+                        }
+
+                }
+        }
+}
+
+class ParameterStringBuilder {
+        public static String getParamsString(Map<String, String> params)
+                        throws UnsupportedEncodingException {
+                StringBuilder result = new StringBuilder();
+
+                for (Map.Entry<String, String> entry : params.entrySet()) {
+                        result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                        result.append("=");
+                        result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+                        result.append("&");
+                }
+
+                String resultString = result.toString();
+                return resultString.length() > 0
+                                ? resultString.substring(0, resultString.length() - 1)
+                                : resultString;
         }
 }
