f555c654e95b6c8b20c18735bfaaa890d095ac19
JnathanCarter <jcarter20@stu.jsu.edu>
2022-11-15 01:30:25 -0600

    part one completed

--- 425-Project-1/src/java/edu/jsu/mcis/dao/RateDAO.java
+++ 425-Project-1/src/java/edu/jsu/mcis/dao/RateDAO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.jsu.mcis.dao;
 
 import java.io.BufferedReader;
@@ -16,19 +12,26 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-
 import javax.swing.text.AbstractDocument.Content;
 import javax.swing.text.html.HTMLDocument.RunElement;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
+import org.json.simple.parser.ContainerFactory;
+import org.json.simple.parser.JSONParser;
 
 /**
  *
  * @author johnc
+ *         In hindsight this code has become complicated and needs simplifying,
+ *         However it seems to work and I need to begin project 2 ASAP,
+ *         My Apologies if it seems a mess, I plan to clean it up during
+ *         Thanksgiving break for my own benefit.
  */
 public class RateDAO {
 
@@ -38,10 +41,19 @@ public class RateDAO {
         private final String QUERY_CREATE = "INSERT INTO rate (currencyid, rate_date, rate)"
                         + "VALUES (?,?,?)";
 
+        /* Constructor */
         RateDAO(DAOFactory daoFactory) {
                 this.daoFactory = daoFactory;
         }
 
+        /**
+         * Queries the database for the rate information
+         * If the information does not exist,
+         * then it is fetched by an external source using updateInternalDB()
+         * 
+         * @param date - String for date of the rates
+         * @return - JSON String containing rate information
+         */
         public String find(String date) {
                 JSONArray jsonArray = new JSONArray();
                 Connection conn = daoFactory.getConnection();
@@ -59,7 +71,7 @@ public class RateDAO {
                         if (hasresults) {
                                 rs = ps.getResultSet();
                                 if (rs.next()) {
-
+                                        // Dianostic Print
                                         System.err.print(rs);
 
                                         Map results = new LinkedHashMap<String, String>();
@@ -72,7 +84,9 @@ public class RateDAO {
                                         }
                                         json.put("rates", results);
 
-                                } else {
+                                }
+                                // If rates for the date is not found then the DB is update from external source
+                                else {
                                         return updateInternalDB(date);
                                 }
                         }
@@ -110,6 +124,13 @@ public class RateDAO {
                 return JSONValue.toJSONString(json);
         }
 
+        /**
+         * Queries external source for additional rate information,
+         * Then updates the local DB & returns this new rate data in JSON format
+         * 
+         * @param date - String for date of the rates
+         * @return - JSON String of rate data
+         */
         public String updateInternalDB(String date) {
 
                 // request from external api
@@ -118,28 +139,54 @@ public class RateDAO {
                 ResultSet rs = null;
 
                 try {
-                    // get information from external api
+                        // HTTP GET request to external api for rate data
                         URL url = new URL("https://testbed.jaysnellen.com:8443/JSUExchangeRatesServer/rates");
                         HttpURLConnection con = (HttpURLConnection) url.openConnection();
                         con.setRequestMethod("GET");
 
                         BufferedReader in = new BufferedReader(new InputStreamReader(
-                        con.getInputStream()));
+                                        con.getInputStream()));
                         String inputLine;
                         String output = "";
                         while ((inputLine = in.readLine()) != null) {
-                        System.out.println(inputLine);
-                                                 output = inputLine;
+                                System.out.println(inputLine);
+                                output = inputLine;
 
                         }
-                        System.out.println("output-----------"+output);
                         in.close();
-                        
-                    //parse the output 
-                    
-                    //add to internal db
 
+                        // Dianostic Print
+                        System.out.println("output-----------" + output);
 
+                        // Parse the output of HTTP Request to external API
+                        JSONParser parser = new JSONParser();
+                        JSONObject json = (JSONObject) parser.parse(output);
+
+                        // Diagnostic Print
+                        Object rates = json.get("rates");
+                        System.out.println(JSONValue.toJSONString(rates));
+
+                        // Make map of rates
+                        ContainerFactory containerFactory = new ContainerFactory() {
+                                @Override
+                                public Map createObjectContainer() {
+                                        return new LinkedHashMap<>();
+                                }
+
+                                @Override
+                                public List creatArrayContainer() {
+                                        return new LinkedList<>();
+                                }
+                        };
+                        Map map = (Map) parser.parse(JSONValue.toJSONString(json.get("rates")), containerFactory);
+
+                        /*
+                         * If the addition to the database is successful,
+                         * the find method is called again to return the newly added data
+                         */
+                        if (addCurrencyData(map, date)) {
+                                this.find(date);
+                        }
 
                 } catch (Exception e) {
                         e.printStackTrace();
@@ -171,30 +218,48 @@ public class RateDAO {
                         }
 
                 }
-                // instead of adding to json object add each entry to the database
 
-                // call find again
-                // return this.find(date);
-                return "";
+                // Use find method to re-Query database that now includes the new data from
+                // extern. API
+                return this.find(date);
         }
 
-        private void addCurrencyData(String date, String currencyId, Double rate) {
+        /**
+         * Accepts Map of rate data and data to add to local DB
+         * 
+         * @param map
+         * @param date
+         * @return
+         */
+        private Boolean addCurrencyData(Map map, String date) {
 
                 PreparedStatement ps = null;
                 ResultSet rs = null;
 
                 try {
-                        Connection conn = daoFactory.getConnection();
 
+                        Connection conn = daoFactory.getConnection();
                         ps = conn.prepareStatement(QUERY_CREATE);
-                        ps.setString(1, currencyId);
-                        ps.setString(2, date);
-                        ps.setDouble(3, rate);
+
+                        Iterator<Map.Entry<String, Object>> itr = map.entrySet().iterator();
+                        while (itr.hasNext()) {
+                                Map.Entry<String, Object> entry = itr.next();
+                                System.out.println("Key = " + entry.getKey() +
+                                                ", Value = " + entry.getValue());
+
+                                ps.setString(1, entry.getKey());
+                                ps.setString(2, date);
+                                ps.setDouble(3, (Double) entry.getValue());
+                                ps.addBatch();
+
+                        }
+                        int[] r = ps.executeBatch();
+                        conn.commit();
 
                         int updateCount = ps.executeUpdate();
 
                         if (updateCount > 0) {
-                                return;
+                                return true;
                         }
 
                 } catch (Exception e) {
@@ -219,24 +284,26 @@ public class RateDAO {
                         }
 
                 }
+                return false;
         }
 }
 
-class ParameterStringBuilder {
-        public static String getParamsString(Map<String, String> params)
-                        throws UnsupportedEncodingException {
-                StringBuilder result = new StringBuilder();
+// Descarded code
 
-                for (Map.Entry<String, String> entry : params.entrySet()) {
-                        result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-                        result.append("=");
-                        result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-                        result.append("&");
-                }
+// class ParameterStringBuilder {
+// public static String getParamsString(Map<String, String> params)
+// throws UnsupportedEncodingException {
+// StringBuilder result = new StringBuilder();
 
-                String resultString = result.toString();
-                return resultString.length() > 0
-                                ? resultString.substring(0, resultString.length() - 1)
-                                : resultString;
-        }
-}
+// for (Map.Entry<String, String> entry : params.entrySet()) {
+// result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+// result.append("=");
+// result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+// result.append("&");
+// }
+
+// String resultString = result.toString();
+// return resultString.length() > 0
+// ? resultString.substring(0, resultString.length() - 1)
+// : resultString;
+// }
